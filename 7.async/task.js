eb498e819598ee722cc431192af0abdcf28a3f96
Anastasiia <105926884+nanasikkk@users.noreply.github.com>
2024-04-03 12:43:45 +0000

    Реализовано домашнее задание по теме: Асинронность

---
+++ 7.async/task.js
@@ -0,0 +1,63 @@
+class AlarmClock {
+    constructor() {
+      this.alarmCollection = [];
+      this.intervalId = null;
+    }
+  
+    addClock(time, callback) {
+      if (!time || !callback) {
+        throw new Error('Отсутствуют обязательные аргументы');
+      }
+  
+      const isSameTimeAlarm = this.alarmCollection.some(alarm => alarm.time === time);
+      if (isSameTimeAlarm) {
+        console.warn('Уже присутствует звонок на это же время');
+      }
+  
+      this.alarmCollection.push({ time, callback, canCall: true });
+    }
+  
+    removeClock(time) {
+      this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time);
+    }
+  
+    getCurrentFormattedTime() {
+      const currentDate = new Date();
+      return currentDate.toLocaleTimeString("ru-Ru", {
+        hour: "2-digit",
+        minute: "2-digit",
+      });
+    }
+  
+    start() {
+      if (this.intervalId) {
+        return;
+      }
+  
+      this.intervalId = setInterval(() => {
+        const currentTime = this.getCurrentFormattedTime();
+        this.alarmCollection.forEach(alarm => {
+          if (alarm.time === currentTime && alarm.canCall) {
+            alarm.canCall = false;
+            alarm.callback();
+          }
+        });
+      }, 1000);
+    }
+  
+    stop() {
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+    }
+  
+    resetAllCalls() {
+      this.alarmCollection.forEach(alarm => {
+        alarm.canCall = true;
+      });
+    }
+  
+    clearAlarms() {
+      this.stop();
+      this.alarmCollection = [];
+    }
+  }
